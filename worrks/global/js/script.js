feb5506f846606e56f10148314613ca70f30f759
webmaster70 <webmastered70@mail.ru>
2021-03-16 00:32:32 +0300

    prof

--- worrks/global/js/script.js
+++ worrks/global/js/script.js
@@ -1,20 +1,22 @@
-
-
-$('.menu__mob-btn').on('click', () => {
-    $('.menu__mob-btn').toggleClass('active');
-    $('.menu').toggleClass('open');
-    $('.menu__items').toggleClass('show');
+$('.menu-header__mob-btn').on('click', () => {
+    $('.menu-header__mob-btn').toggleClass('active');
+    $('.menu-header').toggleClass('open');
+    $('.menu-header__items').toggleClass('show');
 });
 for (let a = 1; a <= $(".menu-header__item").length; a++){ 
-    $(".menu__item:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");    
+    $(".menu-header__item:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");    
 }
-
 
 
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
   // Navigation arrows
   navigation: {
@@ -28,33 +30,12 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-
-
-//  появление элемента на экране дл нажатия и скролинга верх (1600 это растояние
-// с верху) когда появляется элемент         
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 1600) {
-    $('.pageup').fadeIn();
-  } else {
-    $('.pageup').fadeOut();
-  }
-});
-// скролинг в нутри сайта--- назначить a data-jump="id блока куда нужно скролить"
-$(function(){
-$("a[data-jump^='#']").click(function(){
-var _href = $(this).attr("data-jump");
-$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-return false;
-});
-});
-
-
-
-
 $('[data-modal=consultation]').on('click', function(){
   $('.overlay,  #consultation').fadeIn('slow');
   });
- 
+  $('[data-modal=order]').on('click', function(){
+    $('.overlay,  #order').fadeIn('slow');
+    });
   
 
   
@@ -78,6 +59,64 @@ function valideForms(form){
         }
       }
     });
+    
+      $('#form2').validate({
+        rules : {
+          name:  'required',
+          phone: 'required',
+          textarea:'required',
+          email: {
+            required: true,
+            email: true 
+          }
+        },
+        messages: {
+          name: 'пожалуйста, введите ваше имя',
+          phone: 'пожалуйста, введите номер телефона ',
+          checkbox:'пожалуйста, ознакомьтесь с документом ',
+          textarea:  'пожалуйста, введите сообщение ',
+          email:{
+            required: 'пожалуйста, введите адрес почты',
+            email: 'неправильно введен адрес  почты'
+          }
+        }
+      });
+     
+        $('#form3').validate({
+          rules : {
+            name:  'required',
+            phone: 'required',
+            email: {
+              required: true,
+              email: true 
+            }
+          },
+          messages: {
+            name: 'пожалуйста, введите ваше имя',
+            phone: 'пожалуйста, введите номер телефона ',
+            email:{
+              required: 'пожалуйста, введите адрес почты',
+              email: 'неправильно введен адрес  почты'
+            }
+          }
+        });
+       
+          $('#form4').validate({
+            rules : {
+              name:  'required',
+              email: {
+                required: true,
+                email: true 
+              }
+            },
+            messages: {
+              name: 'пожалуйста, введите ваше имя',
+              email:{
+                required: 'пожалуйста, введите адрес почты',
+                email: 'неправильно введен адрес  почты'
+              }
+            }
+          });
   };
   valideForms('form');
   
@@ -95,7 +134,7 @@ $('form').submit(function(e){
     }).done(function(){
       $(this).find("input").val("");
       
-      $('#consultation ').fadeOut('slow');
+      $('#consultation, #order').fadeOut('slow');
       $('.overlay, #thanks').fadeIn('slow');
      
       $('form').trigger('reset');
@@ -105,3 +144,22 @@ $('form').submit(function(e){
    $('.modal__close').on('click', function(){
     $('.overlay,  #thanks').fadeOut('slow') ;
 }) ;
+
+
+//  появление элемента на экране дл нажатия и скролинга верх (1600 это растояние
+// с верху) когда появляется элемент         
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1600) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+});
+// скролинг в нутри сайта--- назначить a data-jump="id блока куда нужно скролить"
+$(function(){
+$("a[data-jump^='#']").click(function(){
+var _href = $(this).attr("data-jump");
+$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+return false;
+});
+});
